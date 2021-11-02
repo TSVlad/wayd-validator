1f0c4b00fafd7760dc19f17b543d918d88a3dcb6
TSVlad <vladislav.kolmykov@yandex.ru>
2021-11-02 11:45:36 +0300

    ages added

--- src/main/java/ru/tsvlad/waydvalidator/messaging/dto/RoleDTO.java
+++ src/main/java/ru/tsvlad/waydvalidator/messaging/dto/RoleDTO.java
@@ -1,9 +1,11 @@
 package ru.tsvlad.waydvalidator.messaging.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RoleDTO {
     private long id;
     private String name;
--- src/main/java/ru/tsvlad/waydvalidator/messaging/dto/UserDTO.java
+++ src/main/java/ru/tsvlad/waydvalidator/messaging/dto/UserDTO.java
@@ -1,10 +1,12 @@
 package ru.tsvlad.waydvalidator.messaging.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
     private long id;
     private String username;
